c4581ed7cae5bfa33e41c4af5836632df505fe30
unknown <junaid.ai@gmail.com>
2026-01-16 00:55:31 +0500

    Class 3

---
+++ Class 3/app.js
@@ -0,0 +1,143 @@
+let fruitList = [
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Date",
+  "Papaya",
+  "Orange",
+  "Grape",
+  "Pine Apple",
+  "Apricot",
+  "Mango",
+];
+
+// // Array Method//
+// // indexof()//
+
+// let position = fruitList.indexOf("Papaya");
+// console.log("The position of Papaya in the fruitList is: " + position);
+
+
+
+// // includes()//
+
+// let hasMango = fruitList.includes("Mango");
+// console.log("Is Mango present in the fruitList? " + hasMango);
+
+
+
+
+// // push()//
+
+// fruitList.push("Kiwi");
+// console.log("Updated fruitList after adding Kiwi: " + fruitList);    
+
+
+
+// // pop()//
+
+// let removedFruit = fruitList.pop();
+// console.log("Removed fruit: " + removedFruit);
+// console.log("Updated fruitList after removing last fruit: " + fruitList);
+
+
+
+// // unshift()//
+
+// fruitList.unshift("Strawberry");
+// console.log("Updated fruitList after adding Strawberry at the beginning: " + fruitList);
+
+
+
+// // shift()//
+
+// let shiftedFruit = fruitList.shift();
+// console.log("Removed fruit from the beginning: " + shiftedFruit);
+// console.log("Updated fruitList after removing first fruit: " + fruitList);
+
+
+// // slice()//
+
+// let citrusFruits = fruitList.slice(5, 7);
+// console.log("Citrus fruits from the fruitList: " + citrusFruits);
+
+
+
+
+// splice()//
+
+// fruitList.splice(2, 1, "Kiwi", "Watermelon");
+// console.log("Updated fruitList after splice operation: " + fruitList);
+
+
+
+
+// // join()//
+
+// let fruitString = fruitList.join(". ");
+// console.log(fruitString + ".");
+
+
+
+
+// // concat()//
+
+// let tropicalFruits = ["Pineapple", "Mango", "Papaya"];
+// let combinedFruits = fruitList.concat(tropicalFruits);
+// console.log("Combined New fruit list: " + combinedFruits);
+
+
+
+
+
+
+// sort()//
+
+// fruitList.sort();
+// console.log("Sorted fruitList: " + fruitList);
+
+
+
+
+// // reverse()//
+
+// fruitList.reverse();
+// console.log("Reversed fruitList: " + fruitList);
+
+// // length()//
+
+// let fruitCount = fruitList.length;
+// console.log("Total number of fruits in the fruitList: " + fruitCount);
+
+
+// Loop //
+
+
+
+// // for loop // //
+
+// for (let i = 0; i < fruitList.length; i++) {
+//   console.log("Fruit at index " + i + ": " + fruitList[i]);
+// }
+
+
+
+
+
+// // for of // 
+
+// for (let element of fruitList) {  
+
+//   console.log(element);
+
+//   }
+
+
+// let text = "PAK"
+// for (let character of text) {  
+
+//   console.log(character);
+
+// }
+
+
